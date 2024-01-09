2310f35c403b0aa9a76aee74a264bcb6cef8bf66
hedys9 <hedysadeghi@gmail.com>
2024-01-09 12:44:33 +0100

    structure finished without bootstrap

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- Gli statement non devono andare a capo
-INSERT INTO pizzas (`name`, description, photo, price) VALUES('Margherita', 'mozzarella,pomodoro', '', 5);
-INSERT INTO pizzas (`name`, description, photo, price) VALUES('Marinara', 'pomodoro,origano', '', 4);
+INSERT INTO pizzas (`name`, description, photo, price) VALUES('Margherita', 'mozzarella,pomodoro', 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Pizza_Margherita_stu_spivack.jpg', 5);
+INSERT INTO pizzas (`name`, description, photo, price) VALUES('Marinara', 'pomodoro,origano', 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/97C2C718-5015-437F-8355-316EE000F57C/Derivates/556409BB-02E0-4BA3-BC16-F409F609FCED.jpg', 4);

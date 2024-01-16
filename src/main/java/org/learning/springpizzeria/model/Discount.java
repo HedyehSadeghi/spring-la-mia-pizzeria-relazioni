f364f5f013014f93c3fb9082d4472c9b7d758442
hedys9 <hedysadeghi@gmail.com>
2024-01-16 17:53:39 +0100

    last commit

--- src/main/java/org/learning/springpizzeria/model/Discount.java
+++ src/main/java/org/learning/springpizzeria/model/Discount.java
@@ -1,7 +1,6 @@
 package org.learning.springpizzeria.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
 
@@ -12,7 +11,6 @@ public class Discount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
-    @NotEmpty(message = "title must not be blank")
     private String title;
     private LocalDate startDate;
     private LocalDate expireDate;

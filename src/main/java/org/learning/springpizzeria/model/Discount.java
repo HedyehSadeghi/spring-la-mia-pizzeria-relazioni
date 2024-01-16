7d881a7cd4d13bb2c44c7bd9bd07e8b05c623faf
hedys9 <hedysadeghi@gmail.com>
2024-01-16 15:18:28 +0100

    discount model

---
+++ src/main/java/org/learning/springpizzeria/model/Discount.java
@@ -0,0 +1,59 @@
+package org.learning.springpizzeria.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "discounts")
+public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String title;
+    private LocalDate startDate;
+    private LocalDate expireDate;
+    @ManyToOne
+    private Pizza pizza;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+}
